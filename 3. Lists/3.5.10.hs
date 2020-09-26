0d29f02781b890bb2196f904ed7a88b4ed7197be
programmer <internetov.ru@gmail.com>
2020-09-26 22:30:21 +0300

    data types

--- 3. Lists/3.5.10.hs
+++ 3. Lists/3.5.10.hs
@@ -1,11 +1,11 @@
-evenOnly :: [a] -> [a]
-evenOnly xs = fst $ foldr (hlp) ([], odd $ length xs) xs
-hlp x acc = (
-        if snd acc then x : (fst acc) else fst acc,
-        not (snd acc)
-    )
+--evenOnly :: [a] -> [a]
+evenOnly = (map snd) . filter (\(n, x) -> even n) . zip [1..]
 tst1 = evenOnly [1..10]
 tst2 = evenOnly [1..9]
-tst3 = take 4 $ (evenOnly [1..])
+tst3 = take 4 $ evenOnly [1..]
 ----
+{-
+take 4 $ evenOnly [1..]
+~>
+-}
 tst = [tst1, tst2, tst3]
