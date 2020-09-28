abec20465b22cabd9833380768a1e9143800b550
programmer <internetov.ru@gmail.com>
2020-09-28 20:52:58 +0300

    data pattern match

--- 3. Lists/3.5.10.hs
+++ 3. Lists/3.5.10.hs
@@ -1,11 +1,10 @@
---evenOnly :: [a] -> [a]
+evenOnly :: [a] -> [a]
 evenOnly = (map snd) . filter (\(n, x) -> even n) . zip [1..]
+evenOnly = foldr (hlp) [([], True)]
+
+hlp x acc = ([x], length acc)
+
 tst1 = evenOnly [1..10]
 tst2 = evenOnly [1..9]
 tst3 = take 4 $ evenOnly [1..]
-----
-{-
-take 4 $ evenOnly [1..]
-~>
--}
 tst = [tst1, tst2, tst3]
