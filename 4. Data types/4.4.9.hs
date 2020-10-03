b1e5ac3d8e2b5c59b95afd9153ef2485584c993a
programmer <internetov.ru@gmail.com>
2020-10-03 22:20:59 +0300

    details about interesting task

--- 4. Data types/4.4.9.hs
+++ 4. Data types/4.4.9.hs
@@ -1,13 +1,32 @@
+{-
+Реализуйте функцию parsePerson, которая разбирает строки вида firstName = John\nlastName = Connor\nage = 30
+и возвращает либо результат типа Person, либо ошибку типа Error.
+
+    Строка, которая подается на вход, должна разбивать по символу '\n' на список строк,
+        каждая из которых имеет вид X = Y. Если входная строка не имеет указанный вид, то функция должна возвращать ParsingError.
+    Если указаны не все поля, то возвращается IncompleteDataError.
+    Если в поле age указано не число, то возвращается IncorrectDataError str, где str — содержимое поля age.
+    Если в строке присутствуют лишние поля, то они игнорируются.
+
+-}
+
+{-
+    на этой задаче сначала ощутил ступор от нехватки мутабельного стейта, но когда родил
+        parsePerson = buildPerson . validateRecords . getRecords . parseStrings
+    все стало понятно, и почувствовалась некоторая легкость и конкретность/выразительность
+-}
+
 import Data.List.Split (splitOn)
 import Data.Char (isDigit)
-data Error = ParsingError | IncompleteDataError | IncorrectDataError String deriving Show
 
+data Error = ParsingError | IncompleteDataError | IncorrectDataError String deriving Show
 data Person = Person { firstName :: String, lastName :: String, age :: Int } deriving Show
+
 type RecordSet = [(String, String)]
 type ParsingResult = Either Error RecordSet
 
 parsePerson :: String -> Either Error Person
-parsePerson = buildPerson . validateRecords . getRecords . items
+parsePerson = buildPerson . validateRecords . getRecords . parseStrings
 
 buildPerson :: ParsingResult -> Either Error Person
 buildPerson (Left e) = Left e
@@ -33,8 +52,8 @@ getRecords ([a, b]:xs) = case getRecords xs of
 getRecords _ = Left ParsingError
 
 
-items :: String -> [[String]]
-items inp = map (splitOn " = ") $ lines inp
+parseStrings :: String -> [[String]]
+parseStrings inp = map (splitOn " = ") $ lines inp
 
 
 has :: RecordSet -> String -> Bool
