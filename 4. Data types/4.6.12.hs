614202d5ecb0f6e70d1551a7f1667f52d63603c1
programmer <internetov.ru@gmail.com>
2020-10-05 00:02:16 +0300

    last data types

---
+++ 4. Data types/4.6.12.hs
@@ -0,0 +1,18 @@
+import Prelude hiding (lookup)
+import qualified Data.List as Lib (lookup)
+class MapLike m where
+    empty :: m k v
+    lookup :: Ord k => k -> m k v -> Maybe v
+    insert :: Ord k => k -> v -> m k v -> m k v
+    delete :: Ord k => k -> m k v -> m k v
+    fromList :: Ord k => [(k,v)] -> m k v
+
+newtype ArrowMap k v = ArrowMap { getArrowMap :: k -> Maybe v }
+
+instance MapLike ArrowMap where
+    empty = ArrowMap (\ _ -> Nothing)
+    lookup k (ArrowMap f) = f k
+    insert k v (ArrowMap f) = ArrowMap (\x -> if x == k then Just v else f x)
+    delete k (ArrowMap f) = ArrowMap (\x -> if x == k then Nothing else f x)
+    fromList xs = ArrowMap (\k -> Lib.lookup k xs)
+
