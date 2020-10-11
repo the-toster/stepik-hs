1cff7b674339f3458cedaca31bbfe96062ba9a04
programmer <internetov.ru@gmail.com>
2020-10-11 14:29:47 +0300

    RWH chapter one task

--- Real World Haskell/1. Getting Started/wc.hs
+++ Real World Haskell/1. Getting Started/wc.hs
@@ -1,6 +1,8 @@
 main = interact (\input -> show (lineCount input) ++ "\n")
 
 lineCount =  length . lines
-inp = "Teignmouth, England\nParis, France\nUlm, Germany\nAuxerre, France\nBrunswick, Germany\nBeaumont-en-Auge, France\nRyazan, Russia";
 wordCount input = sum $ map (length . words) (lines input)
-tst = wordCount inp
+charCount inp = sum $ map length (words inp)
+
+inp = "Teignmouth, England\nParis, France\nUlm, Germany\nAuxerre, France\nBrunswick, Germany\nBeaumont-en-Auge, France\nRyazan, Russia";
+tst = charCount inp
