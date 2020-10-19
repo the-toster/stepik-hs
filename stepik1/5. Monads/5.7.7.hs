f3c099e36de6a8b1eccd412915a0ff01b3c140f2
programmer <internetov.ru@gmail.com>
2020-10-19 23:32:20 +0300

    Writer..

---
+++ stepik1/5. Monads/5.7.7.hs
@@ -0,0 +1,34 @@
+import Data.Monoid
+
+newtype Writer w a = Writer { runWriter :: (a, w) } deriving Show
+
+writer :: (a, w) -> Writer w a
+writer = Writer
+
+execWriter :: Writer w a -> w
+execWriter m = snd (runWriter m)
+
+instance (Monoid w) => Monad (Writer w) where
+    return x = Writer (x, mempty)
+    m >>= k  =
+        let (x, u) = runWriter m
+            (y, v) = runWriter $ k x
+        in Writer (y, u `mappend` v)
+
+type Shopping = Writer (Sum Integer) ()
+
+purchase :: String -> Integer -> Shopping
+purchase item cost = writer ((), Sum cost)
+
+shopping1 :: Shopping
+shopping1 = do
+  purchase "Jeans"   19200
+  purchase "Water"     180
+  purchase "Lettuce"   328
+
+
+total :: Shopping -> Integer
+total = getSum . execWriter
+
+items :: Shopping -> [String]
+items = undefined
