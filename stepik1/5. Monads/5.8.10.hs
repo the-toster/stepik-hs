60e12154e8bf8d4b29025f7888268fb53817d73b
programmer <internetov.ru@gmail.com>
2020-10-22 11:18:51 +0300

    State last - looking in correct solution and reproduce

--- stepik1/5. Monads/5.8.10.hs
+++ stepik1/5. Monads/5.8.10.hs
@@ -1,3 +1,25 @@
+newtype State s a = State { runState :: s -> (a,s) }
+-- runState :: State s a -> s -> (a,s)
+
+instance Monad (State s) where
+    return a = State $ \st -> (a, st)
+    m >>= k  = State $ \st ->
+        let (a, st') = runState m st
+            m' = k a
+        in runState m' st'
+
+get :: State s s
+get = State $ \st -> (st, st)
+
+put :: s -> State s ()
+put st = State $ \_ -> ((), st)
+
+execState :: State s a -> s -> s
+execState m s = snd (runState m s)
+
+evalState :: State s a -> s -> a
+evalState m s = fst (runState m s)
+
 data Tree a = Leaf a | Fork (Tree a) a (Tree a) deriving (Eq, Show)
 
 tst = [
@@ -6,8 +28,20 @@ tst = [
     ]
 
 numberTree :: Tree () -> Tree Integer
-numberTree tree = undefined
+numberTree t = evalState (nTree t) 1
 
+nTree :: Tree () -> State Integer (Tree Integer)
+nTree (Leaf ()) = do
+    n <- get
+    put (n + 1)
+    return (Leaf n)
+
+nTree (Fork l () r) = do
+    ln <- nTree l
+    n  <- get
+    put (n + 1)
+    rn <- nTree r
+    return (Fork ln n rn)
 
 -- первая версия, проходит тесты, чтобы проверить, что я правильно понимаю общее решение
 --numberTree tree = fst $ nTree 0 tree
