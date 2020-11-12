b27cf0362f956e83156a94452f75bc6ed8f76401
programmer <internetov.ru@gmail.com>
2020-11-12 23:08:50 +0300

    foldable composition complete

--- stepik2/2. Foldable type class/2.1.16.hs
+++ stepik2/2. Foldable type class/2.1.16.hs
@@ -1,8 +1,11 @@
 {-# LANGUAGE TypeOperators #-}
-import Data.Monoid
 
 infixr 9 |.|
 newtype (|.|) f g a = Cmps {getCmps :: f (g a)} deriving (Eq, Show)
 
---instance (Foldable f, Foldable g) => Foldable (f |.| g) where
---    foldMap f (Cmps x) = Cmps $ foldMap (Endo $ foldMap f) _
+instance (Foldable f, Foldable g) => Foldable (f |.| g) where
+    foldMap am (Cmps x) = foldMap (\ga -> foldMap am ga) x
+tst = [
+        (maximum $ Cmps [Nothing, Just 2, Just 3]) == 3,
+        (length $ Cmps [[1,2], [], [3,4,5,6,7]]) == 7
+    ]
