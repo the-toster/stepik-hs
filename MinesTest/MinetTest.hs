e05b4ed97c496adda4ffe8c43725c9955cd20a6d
programmer <internetov.ru@gmail.com>
2020-11-14 23:51:51 +0300

    test on small map

--- MinesTest/MinetTest.hs
+++ MinesTest/MinetTest.hs
@@ -6,28 +6,39 @@ import MinesOrDieAndrey as A -- Андрея
 import EscapeTheMinesOrDie as B (solve) -- Мое
 
 
-
+start = (0, 0)
+door1 = (11, 11)
+door2 = (3, 0)
 
 -- это запустить в ghci
-drw = visualSolve B.solve charMap (0, 0) (10, 10)
+drw = visualSolve A.solve charMap2 start door2
+--drw = visualSolve B.solve charMap (0, 0) (3, 10)
 
 -- рисовалка, там можно вызвать transpose, перед prettyPrint, чтобы повернуть карту, но все равно непонятно выглядит
 visualSolve solver charMap start door =
     let
         m = unmap charMap
         solution = solver m start door
+        visual = \m -> prettyPrint $ drawSolution charMap start door m
     in case solution of
-        Nothing -> putStr "No solution found"
-        Just moves -> putStr $ prettyPrint $ drawSolution charMap start door moves
+        Nothing -> putStrLn $ "No solution found\n" ++ (visual [])
+        Just moves -> putStrLn $ visual moves
 
 -- тесты последнего лабиринта
 tst = [
-        B.solve m  (0, 0) (11, 11) == Just [D,D,R,R,U,U,R,R,D,D,R,R,U,U,R,R,D,D,R,R,R,D,D,L,D,L,L,L,U,L,L,D,L,L,U,L,L,D,L,D,D,R,D,D,L,D,D,R,R,R,U,U,U,U,R,R,D,D,R,R,D,R,R,R,R,D],
-        A.solve m  (0, 0) (11, 11) == Just [D,D,R,R,U,U,R,R,D,D,R,R,U,U,R,R,D,D,R,R,R,D,D,L,D,L,L,L,U,L,L,D,L,L,U,L,L,D,L,D,D,R,D,D,L,D,D,R,R,R,U,U,U,U,R,R,D,D,R,R,D,R,R,R,R,D],
-        B.solve m  (0, 0) (10, 10) == Just [D,D,R,R,U,U,R,R,D,D,R,R,U,U,R,R,D,D,R,R,R,D,D,L,D,L,L,L,U,L,L,D,L,L,U,L,L,D,L,D,D,R,D,D,L,D,D,R,R,R,U,U,U,U,R,R,D,D,R,R,D,R,R,R],
-        A.solve m  (0, 0) (10, 10) == Just [D,D,R,R,U,U,R,R,D,D,R,R,U,U,R,R,D,D,R,R,R,D,D,L,D,L,L,L,U,L,L,D,L,L,U,L,L,D,L,D,D,R,D,D,L,D,D,R,R,R,U,U,U,U,R,R,D,D,R,R,D,R,R,R]
+        (B.solve m2 start door2) == (A.solve m2 start door2)
+--        B.solve m  (0, 0) (11, 11) == Just [D,D,R,R,U,U,R,R,D,D,R,R,U,U,R,R,D,D,R,R,R,D,D,L,D,L,L,L,U,L,L,D,L,L,U,L,L,D,L,D,D,R,D,D,L,D,D,R,R,R,U,U,U,U,R,R,D,D,R,R,D,R,R,R,R,D],
+--        A.solve m  (0, 0) (11, 11) == Just [D,D,R,R,U,U,R,R,D,D,R,R,U,U,R,R,D,D,R,R,R,D,D,L,D,L,L,L,U,L,L,D,L,L,U,L,L,D,L,D,D,R,D,D,L,D,D,R,R,R,U,U,U,U,R,R,D,D,R,R,D,R,R,R,R,D],
+--        B.solve m  (0, 0) (10, 10) == Just [D,D,R,R,U,U,R,R,D,D,R,R,U,U,R,R,D,D,R,R,R,D,D,L,D,L,L,L,U,L,L,D,L,L,U,L,L,D,L,D,D,R,D,D,L,D,D,R,R,R,U,U,U,U,R,R,D,D,R,R,D,R,R,R],
+--        A.solve m  (0, 0) (10, 10) == Just [D,D,R,R,U,U,R,R,D,D,R,R,U,U,R,R,D,D,R,R,R,D,D,L,D,L,L,L,U,L,L,D,L,L,U,L,L,D,L,D,D,R,D,D,L,D,D,R,R,R,U,U,U,U,R,R,D,D,R,R,D,R,R,R]
       ]
 
+charMap2 =               [
+                           "  "
+                          ," #"
+                          ,"  "
+                          ," #"
+                          ]
 
 charMap =               ["   ##   #   "
                           ,"## #  #   # "
@@ -43,6 +54,7 @@ charMap =               ["   ##   #   "
                           ,"#    #  #   "]
 
 m = unmap charMap
+m2 = unmap charMap2
 unmap = map (map (== ' '))
 
 prettyPrint :: [String] -> String
